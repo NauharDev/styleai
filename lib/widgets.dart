cdcc669ce988f4deaa2cddb9b34f855ab6114b71
NauharDev <nauhar.kapur@mail.utoronto.ca>
2023-05-15 23:48:11 -0400

    Completed Sign-in/Sign-up functionality
    completed sign-in and sign-up functionality using Firebase authentication and added UI elements to display any errors during the sign in process. Began working on a pop-up menu to display sign out buttons and other features.

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -21,164 +21,236 @@ class _SignInTemplateState extends State<SignInTemplate> {
   final confirmController = TextEditingController();
   bool invalidInput = false;
   bool signInError = false;
+  String errorText = '';
 
-
-    Future signIn() async {
+  Future signUp() async {
     showDialog(
-      context: context, 
+      context: context,
+      builder: (context) {
+        return const Center(child: CircularProgressIndicator());
+      },
+    );
+
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: emailController.text.trim(),
+          password: passController.text.trim());
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        setState(() {
+          signInError = true;
+          errorText =
+              'The given email address is already in use. Please use a different email.';
+        });
+      } else if (e.code == 'invalid-email') {
+        setState(() {
+          signInError = true;
+          errorText = 'The given email address is invalid. Please try again.';
+        });
+      } else if (e.code == 'weak-password') {
+        setState(() {
+          signInError = true;
+          errorText =
+              'The given password is too weak. Please use a stronger password.';
+        });
+      }
+    }
+    navigatorKey.currentState!.pop();
+  }
+
+  Future signIn() async {
+    showDialog(
+      context: context,
       builder: (context) {
         return const Center(child: CircularProgressIndicator());
       },
     );
     try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: emailController.text.trim(),
-        password: passController.text.trim()
-      );
+          email: emailController.text.trim(),
+          password: passController.text.trim());
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'invalid-email') {
+        setState(() {
+          signInError = true;
+          errorText = 'The given email address is invalid. Please try again.';
+        });
+      } else if (e.code == 'user-disabled') {
+        setState(() {
+          signInError = true;
+          errorText =
+              'The given user is disabled. Please sign in with a different account.';
+        });
+      } else if (e.code == 'user-not-found') {
+        setState(() {
+          signInError = true;
+          errorText =
+              'There is no user associated with the given email. Please try again.';
+        });
+      } else {
+        setState(() {
+          signInError = true;
+          errorText = 'The given password is incorrect. Please try again.';
+        });
+      }
     }
-    on FirebaseAuthException {
-      setState(() {
-        signInError = true;
-      });
-    }
-    navigatorKey.currentState!.popUntil((route) => route.isFirst,);
-    print('i like cheese');
+    navigatorKey.currentState!.pop();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('StyleAI', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),)),
+      appBar: AppBar(
+          title: const Text(
+        'StyleAI',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      )),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Spacer(flex: 1),
-              Text(widget.newUser ? 'Sign Up' : 'Log In', style: Theme.of(context).textTheme.bodyLarge),
-              const Spacer(flex: 4),
-              Visibility(
-                visible: invalidInput || signInError,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.red
-                  ),
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    invalidInput ? 'All fields are required.' : 'Invalid sign-in credentials. Please try again.',
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
+            Text(widget.newUser ? 'Sign Up' : 'Log In',
+                style: Theme.of(context).textTheme.bodyLarge),
+            const Spacer(flex: 4),
+            Visibility(
+              visible: invalidInput || signInError,
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10), color: Colors.red),
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  invalidInput ? 'All fields are required.' : errorText,
+                  style: Theme.of(context).textTheme.bodyMedium,
                 ),
               ),
-               SizedBox(
-                width: MediaQuery.of(context).size.width - 20.0,
-                child: TextField(
-                  controller: emailController,
-                  autocorrect: false,
-                  decoration: const InputDecoration(
-                    hintText: 'email'
-                  ),
-                  onChanged: (value) {
-                    if (emailController.text == '' || passController.text == '' || ( widget.newUser && confirmController.text == '')) {
-                      return;
-                    }
-                    else {
-                      setState(() {
-                        invalidInput = false;
-                      });
-                    }
-                  },
-                ),
-              ),
-               SizedBox(
-                width: MediaQuery.of(context).size.width - 20.0,
-                child: TextField(
-                  controller: passController,
-                  autocorrect: false,
-                  decoration: const InputDecoration(
-                    hintText: 'password'
-                  ),
-                  onChanged: (value) {
-                    if (emailController.text == '' || passController.text == '' || ( widget.newUser && confirmController.text == '')) {
-                      return;
-                    }
-
-                    else {
-                      setState(() {
-                        invalidInput = false;
-                      });
-                    }
-                  },
-                ),
-              ),
-              Visibility(
-                visible: widget.newUser,
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width - 20.0,
-                  child: TextField(
-                    controller: confirmController,
-                    autocorrect: false,
-                    decoration: const InputDecoration(
-                      hintText: 'confirm password', 
-                    ),
-                    onChanged: (value) {
-                    if (emailController.text == '' || passController.text == '' || ( widget.newUser && confirmController.text == '')) {
-                      return;
-                    }
-                    else {
-                      setState(() {
-                        invalidInput = false;
-                      });
-                    }
-                    },
-                  ),
-                ),
-              ),
-              TextButton(
-                child: Text(widget.newUser ? 'Have an account? Click here to log in!' : 'Don\'t have an account? Click here to sign up!'), 
-                onPressed: () {
-                  widget.newUser ? Navigator.pop(context) : 
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => SignInTemplate(newUser: !widget.newUser,),));
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 20.0,
+              child: TextField(
+                controller: emailController,
+                autocorrect: false,
+                decoration: const InputDecoration(hintText: 'email'),
+                onChanged: (value) {
+                  if (emailController.text == '' ||
+                      passController.text == '' ||
+                      (widget.newUser && confirmController.text == '')) {
+                    return;
+                  } else {
+                    setState(() {
+                      invalidInput = false;
+                    });
+                  }
                 },
               ),
-            Stack(
-              children: [
-                Positioned.fill(
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      gradient: LinearGradient(
-                        colors: [
-                          Color(0xFF0D47A1),
-                          Color(0xFF1976D2),
-                          Color(0xFF42A5F5),
-                        ]
-                      )
-                    )
-                  )
-                ),
-                TextButton(
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.white
-                ),
-                child: const Text('Submit', style: TextStyle(fontSize: 18.0)),
-                onPressed: ()  {
-                  if (emailController.text == '' || passController.text == '' || ( widget.newUser && confirmController.text == '')) {
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 20.0,
+              child: TextField(
+                controller: passController,
+                autocorrect: false,
+                decoration: const InputDecoration(hintText: 'password'),
+                onChanged: (value) {
+                  if (emailController.text == '' ||
+                      passController.text == '' ||
+                      (widget.newUser && confirmController.text == '')) {
+                    return;
+                  } else {
                     setState(() {
-                      invalidInput = true;
-                  },);
+                      invalidInput = false;
+                    });
                   }
-                  else if (widget.newUser) {
-                    //create new account
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen(globals.cameras),));
-                  }
-                  else {
-                    signIn();
-                  }
+                  if (passController.text.trim() == confirmController.text.trim()) {
+                      setState(() {
+                        signInError = false;
+                      });
+                    }
+                },
+              ),
+            ),
+            Visibility(
+              visible: widget.newUser,
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width - 20.0,
+                child: TextField(
+                  controller: confirmController,
+                  autocorrect: false,
+                  decoration: const InputDecoration(
+                    hintText: 'confirm password',
+                  ),
+                  onChanged: (value) {
+                    if (emailController.text == '' ||
+                        passController.text == '' ||
+                        (widget.newUser && confirmController.text == '')) {
+                      return;
+                    } else {
+                      setState(() {
+                        invalidInput = false;
+                      });
+                    }
+                    if (passController.text.trim() == confirmController.text.trim()) {
+                      setState(() {
+                        signInError = false;
+                      });
+                    }
                   },
                 ),
-              ]
+              ),
             ),
+            TextButton(
+              child: Text(widget.newUser
+                  ? 'Have an account? Click here to log in!'
+                  : 'Don\'t have an account? Click here to sign up!'),
+              onPressed: () {
+                widget.newUser
+                    ? Navigator.pop(context)
+                    : Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SignInTemplate(
+                            newUser: !widget.newUser,
+                          ),
+                        ));
+              },
+            ),
+            Stack(children: [
+              Positioned.fill(
+                  child: Container(
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          gradient: LinearGradient(colors: [
+                            Color(0xFF0D47A1),
+                            Color(0xFF1976D2),
+                            Color(0xFF42A5F5),
+                          ])))),
+              TextButton(
+                style: TextButton.styleFrom(foregroundColor: Colors.white),
+                child: const Text('Submit', style: TextStyle(fontSize: 18.0)),
+                onPressed: () {
+                  if (emailController.text == '' ||
+                      passController.text == '' ||
+                      (widget.newUser && confirmController.text == '')) {
+                    setState(
+                      () {
+                        invalidInput = true;
+                      },
+                    );
+                  } else if (widget.newUser) {
+                    if (confirmController.text.trim() !=
+                        passController.text.trim()) {
+                      setState(() {
+                        signInError = true;
+                        errorText = 'Passwords don\'t match.';
+                      });
+                    } else {
+                      signUp();
+                    }
+                  } else {
+                    signIn();
+                  }
+                },
+              ),
+            ]),
             const Spacer(flex: 2),
             // Flexible(
             //   fit: FlexFit.tight,
@@ -194,7 +266,7 @@ class _SignInTemplateState extends State<SignInTemplate> {
             //             borderRadius: BorderRadius.all(Radius.circular(10)),
             //             color: Colors.red,
             //           ),
-            //           child: Text('Invalid sign-in credentials. Please try again.', 
+            //           child: Text('Invalid sign-in credentials. Please try again.',
             //             style: Theme.of(context).textTheme.bodyMedium,),
             //         );
             //       }
@@ -204,7 +276,6 @@ class _SignInTemplateState extends State<SignInTemplate> {
             //     },
             //   ),
             // )
-
           ],
         ),
       ),
